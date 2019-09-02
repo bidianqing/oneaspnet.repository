20bfda8fd291d38dafcddc5423f0b2111697bda9
毕殿卿 <bdq123@outlook.com>
2019-09-02 16:38:20 +0800

    *

--- src/OneAspNet.Repository.Dapper/DbProviderFactories.cs
+++ src/OneAspNet.Repository.Dapper/DbProviderFactories.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Data.Common;
 using System.Reflection;
 
@@ -7,20 +8,15 @@ namespace OneAspNet.Repository.Dapper
 {
     public static class DbProviderFactories
     {
-        private static DbProviderFactory _sqlFactory = null;
-        private static DbProviderFactory _mysqlFactory = null;
-        private static DbProviderFactory _sqliteFactory = null;
         private static ConcurrentDictionary<string, DbProviderFactory> _factories = new ConcurrentDictionary<string, DbProviderFactory>(StringComparer.OrdinalIgnoreCase);
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="providerName">        
-        /// 数据库工厂类型, 需要引用对应的数据库client库, 
-        /// 已知工厂类型 MySql,SqlServer,Sqlite,
-        /// 未知类型使用全限定类型名，如（"MySql.Data.MySqlClient.MySqlClientFactory, MySqlConnector"）
-        /// </param>        
-        /// <returns></returns>
+        private static Dictionary<string, string> _drives = new Dictionary<string, string>()
+        {
+            { "MySql","MySql.Data.MySqlClient.MySqlClientFactory, MySqlConnector"},
+            { "SqlServer","Microsoft.Data.SqlClient.SqlClientFactory, Microsoft.Data.SqlClient"},
+            { "Sqlite","Microsoft.Data.Sqlite.SqliteFactory, Microsoft.Data.Sqlite"},
+        };
+
         public static DbProviderFactory GetFactory(string providerName)
         {
             if (string.IsNullOrWhiteSpace(providerName))
@@ -28,66 +24,41 @@ namespace OneAspNet.Repository.Dapper
                 throw new ArgumentException("is null or empty", providerName);
             }
 
-            if (string.Equals(providerName, "MySql", StringComparison.OrdinalIgnoreCase))
+            return _factories.GetOrAdd(providerName, (pName) =>
             {
-                if (_mysqlFactory == null)
-                {
-                    _mysqlFactory = CreateFactory("MySql.Data.MySqlClient.MySqlClientFactory, MySqlConnector");
-                }
-                return _mysqlFactory;
-            }
-
-            if (string.Equals(providerName, "SqlServer", StringComparison.OrdinalIgnoreCase))
-            {
-                if (_sqlFactory == null)
-                {
-                    _sqlFactory = CreateFactory("System.Data.SqlClient.SqlClientFactory, System.Data.SqlClient");
-                }
-                return _sqlFactory;
-            }
-
-            if (string.Equals(providerName, "Sqlite", StringComparison.OrdinalIgnoreCase))
-            {
-                if (_sqliteFactory == null)
-                {
-                    _sqliteFactory = CreateFactory("Microsoft.Data.Sqlite.SqliteFactory, Microsoft.Data.Sqlite");
-                }
-                return _sqliteFactory;
-            }
-
-            return _factories.GetOrAdd(providerName, CreateFactory);
+                return CreateFactory(_drives[pName]);
+            });
         }
+
 
         private static DbProviderFactory CreateFactory(string typeName)
         {
-            DbProviderFactory val = null;
             var type = Type.GetType(typeName);
-            if (type != null)
+
+            if (type == null)
             {
-                var propertyInfo = type.GetTypeInfo().GetProperty("Instance");
-                if (propertyInfo != null)
-                {
-                    val = propertyInfo.GetValue(type) as DbProviderFactory;
-                }
-
-                if (val == null)
-                {
-                    var fieldInfo = type.GetTypeInfo().GetField("Instance");
-                    val = fieldInfo.GetValue(type) as DbProviderFactory;
-                }
-
-                if (val == null)
-                {
-                    val = Activator.CreateInstance(type) as DbProviderFactory;
-                }
+                throw new ArgumentException($"{typeName} can not load");
             }
 
-            if (val == null)
+            var providerInstance = type.GetField("Instance", BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Static);
+
+            if (providerInstance == null)
             {
-                throw new TypeLoadException(typeName);
+                throw new InvalidOperationException(typeName);
             }
 
-            return val;
+            if (!providerInstance.FieldType.IsSubclassOf(typeof(DbProviderFactory)))
+            {
+                throw new InvalidOperationException(typeName);
+            }
+
+            object factory = providerInstance.GetValue(null);
+            if (null == factory)
+            {
+                throw new InvalidOperationException(typeName);
+            }
+
+            return (DbProviderFactory)factory;
         }
     }
 }
